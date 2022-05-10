4a1acfaf77fd8b652c3802c484357cdc61208f44
Jhulio-Prando <jhulioprando@gmail.com>
2022-05-10 16:39:44 -0300

    Sistema de cadastro de peças finalizado

---
+++ pecas.js
@@ -0,0 +1,25 @@
+let PesoPeca = 150
+    if(PesoPeca >= 100) {
+        console.log("Peso maior que 100g, podemos cadastrar")
+    } 
+    else {
+        console.log("Peso insuficiente não podemos cadastrar!")
+}
+
+let NumeroPeca = 10
+    if(NumeroPeca <= 10) {
+        console.log("O numero maximo de peças que podem ser cadastrada são 10")
+    }
+    else {
+        console.log("Numero de peças não podem exceder 10 peças, não podemos cadastrar")
+}
+
+let caractere = "Di"
+    console.log("O numero de caracteres e:", caractere.length)
+    if(caractere.length < 3) {
+        console.log("Não é possivel cadastrar")
+    }
+    else {
+        console.log("Podemos cadastrar")
+    }
+    
